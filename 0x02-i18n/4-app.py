39819260d95757c13f0e6efe2502be973181f2e7
ndegz <ndegwa8781@gmail.com>
2023-09-01 23:04:03 +0300

    user invoked config

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -23,13 +23,16 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """specifies client locale and language preference"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def hello():
     """flush template"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
